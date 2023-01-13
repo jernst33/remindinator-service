993f0f86d133585ddcbc7523fa6d1a811808dfe1
Jacob Ernst <jernst4601@gmail.com>
2023-01-12 20:02:15 -0500

    enable basic authentication for single endpoint

--- src/main/java/PublicResource.java
+++ src/main/java/PublicResource.java
@@ -1,9 +1,11 @@
 import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import java.util.LinkedList;
+import javax.ws.rs.core.SecurityContext;
 import java.util.List;
 
 @Path("/api/public")
@@ -16,16 +18,37 @@ public class PublicResource {
     @PermitAll
     @Produces(MediaType.APPLICATION_JSON)
     public List<User> getAllUsers() {
-       return userRepository.listAll();
+        return userRepository.listAll();
     }
 
     @GET
     @Path("{userId}")
-    @PermitAll
+    @RolesAllowed({"user", "admin"})
     @Produces(MediaType.APPLICATION_JSON)
     public User getUser(@PathParam("userId") Long userId) {
         return userRepository.findById(userId);
     }
+
+    @GET
+    @Path("who-am-i")
+    @RolesAllowed({"user", "admin", "owner"})
+    @Produces(MediaType.APPLICATION_JSON)
+    public User whoAmI(@Context SecurityContext securityContext) {
+
+        return userRepository.find("username", securityContext.getUserPrincipal().getName()).firstResult();
+//        String response = "";
+//        if (securityContext.isUserInRole("user")) {
+//            response = "You're just a user!";
+//        } else if (securityContext.isUserInRole("owner")) {
+//            response = "Wow, you own this thing.... Maybe we should give you more respect.";
+//        } else if (securityContext.isUserInRole("admin")) {
+//            response = "Wow, you're an admin!";
+//        }
+//
+//        response += " Welcome, " + securityContext.getUserPrincipal().getName();
+//        return response;
+    }
+
 
     @POST
     @PermitAll
